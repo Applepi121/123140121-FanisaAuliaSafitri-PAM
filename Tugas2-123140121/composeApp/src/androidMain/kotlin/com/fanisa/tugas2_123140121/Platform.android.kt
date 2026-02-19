f7011f94d7b4e561826976c16b164d5649dd9a11
Fanisa Aulia Safitri <fanisaaulia26@gmail.com>
2026-02-20 02:22:09 +0700

    Delete Tugas2-123140121/composeApp/src directory

--- Tugas2-123140121/composeApp/src/androidMain/kotlin/com/fanisa/tugas2_123140121/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.fanisa.tugas2_123140121
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- Tugas2-123140121/composeApp/src/commonMain/kotlin/com/fanisa/tugas2_123140121/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.fanisa.tugas2_123140121
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
