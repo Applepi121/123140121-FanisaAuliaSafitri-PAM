afccfaa6e95e9585f2835c7d67028e5c079b99b1
Fanisa Aulia Safitri <fanisaaulia26@gmail.com>
2026-04-26 20:57:35 +0700

    Add files via upload

---
+++ Tugas8_123140121_PAM/composeApp/src/iosMain/kotlin/com/fanisa/upgradenote/data/database/DatabaseDriverFactory.ios.kt
@@ -0,0 +1,14 @@
+package com.fanisa.upgradenote.data.database
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import com.itera.notes.db.NotesDatabase
+
+actual class DatabaseDriverFactory {
+    actual fun createDriver(): SqlDriver {
+        return NativeSqliteDriver(
+            schema = NotesDatabase.Schema,
+            name = "notes.db"
+        )
+    }
+}
